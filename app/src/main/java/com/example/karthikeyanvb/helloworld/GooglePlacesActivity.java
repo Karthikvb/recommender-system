2bd75665cdb8c760a08bbcc0bd90663d0d2badf1
Karthikeyan_V_B <kartmuhil@gmail.com>
2015-05-03 19:12:09 +0530

    first commit

---
+++ app/src/main/java/com/example/karthikeyanvb/helloworld/GooglePlacesActivity.java
@@ -0,0 +1,195 @@
+package com.example.karthikeyanvb.helloworld;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+import com.google.android.gms.maps.GoogleMap;
+
+public class GooglePlacesActivity extends FragmentActivity implements LocationListener {
+
+    public class PlacesDisplayTask extends AsyncTask<Object, Integer, List<HashMap<String, String>>> {
+
+        JSONObject googlePlacesJson;
+        GoogleMap googleMap;
+
+        @Override
+        protected List<HashMap<String, String>> doInBackground(Object... inputObj) {
+
+            List<HashMap<String, String>> googlePlacesList = null;
+            Places placeJsonParser = new Places();
+
+            try {
+                googleMap = (GoogleMap) inputObj[0];
+                googlePlacesJson = new JSONObject((String) inputObj[1]);
+                googlePlacesList = placeJsonParser.parse(googlePlacesJson);
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return googlePlacesList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> list) {
+            googleMap.clear();
+            for (int i = 0; i < list.size(); i++) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                HashMap<String, String> googlePlace = list.get(i);
+                double lat = Double.parseDouble(googlePlace.get("lat"));
+                double lng = Double.parseDouble(googlePlace.get("lng"));
+                String placeName = googlePlace.get("place_name");
+                String vicinity = googlePlace.get("vicinity");
+                LatLng latLng = new LatLng(lat, lng);
+                markerOptions.position(latLng);
+                markerOptions.title(placeName + " : " + vicinity);
+                googleMap.addMarker(markerOptions);
+            }
+        }
+    }
+
+
+    public class GooglePlacesReadTask extends AsyncTask<Object, Integer, String> {
+        String googlePlacesData = null;
+        GoogleMap googleMap;
+
+        @Override
+        protected String doInBackground(Object... inputObj) {
+            try {
+                googleMap = (GoogleMap) inputObj[0];
+                String googlePlacesUrl = (String) inputObj[1];
+                Http http = new Http();
+                googlePlacesData = http.read(googlePlacesUrl);
+            } catch (Exception e) {
+                Log.d("Google Place Read Task", e.toString());
+            }
+            return googlePlacesData;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            PlacesDisplayTask placesDisplayTask = new PlacesDisplayTask();
+            Object[] toPass = new Object[2];
+            toPass[0] = googleMap;
+            toPass[1] = result;
+            placesDisplayTask.execute(toPass);
+        }
+    }
+
+    private static final String GOOGLE_API_KEY = "AIzaSyCyemBFPXAM-OkfKTK0vXJ8NqjRDqW1NTo";
+    GoogleMap googleMap;
+    EditText placeText;
+    double latitude = 0;
+    double longitude = 0;
+    private int PROXIMITY_RADIUS = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //show error dialog if GoolglePlayServices not available
+        if (!isGooglePlayServicesAvailable()) {
+            finish();
+        }
+        setContentView(R.layout.activity_google_places);
+
+        placeText = (EditText) findViewById(R.id.placeText);
+        Button btnFind = (Button) findViewById(R.id.btnFind);
+        SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.googleMap);
+        googleMap = fragment.getMap();
+        googleMap.setMyLocationEnabled(true);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(bestProvider, 20000, 0, this);
+
+        btnFind.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String type = placeText.getText().toString();
+                StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                googlePlacesUrl.append("location=" + latitude + "," + longitude);
+                googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
+                googlePlacesUrl.append("&types=" + type);
+                googlePlacesUrl.append("&sensor=true");
+                googlePlacesUrl.append("&key=" + GOOGLE_API_KEY);
+
+                GooglePlacesReadTask googlePlacesReadTask = new GooglePlacesReadTask();
+                Object[] toPass = new Object[2];
+                toPass[0] = googleMap;
+                toPass[1] = googlePlacesUrl.toString();
+                googlePlacesReadTask.execute(toPass);
+            }
+        });
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+}
